7b48b14f6663e3e1f75697557ab5664c4e9a00b3
lenartow <lenartow@PL1W7L-105987.ttg.global>
2017-06-04 19:53:33 +0200

    hello git

---
+++ src/test/java/lenartow/learn/HelloGitTest.java
@@ -0,0 +1,14 @@
+package lenartow.learn;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloGitTest {
+
+	@Test
+	public final void test() {
+		assertTrue(true);
+	}
+
+}
